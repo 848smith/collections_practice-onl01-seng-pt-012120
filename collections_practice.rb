7a17e95daa14f6b7fde96833b73fa50f959fc4e8
Andrew Smith <848smith@gmail.com>
2020-02-03 09:32:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,30 @@ end
 def sort_array_char_count(array)
   
 end
+
+def swap_elements(array)
+  
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  
+end
+
+def find_a(array)
+  
+end
+
+def sum_array(array)
+  
+end
+
+def add_s(array)
+
+end
+
+
+
